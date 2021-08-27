6e0944654b29ead300bef910d022ef36e6327c91
André <andre1993andre@hotmail.com>
2021-08-27 14:02:08 -0300

    corrigindo identação

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,25 +6,21 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
-	
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		try {
 			String[] vect = sc.nextLine().split(" ");
 			int position = sc.nextInt();
 			System.out.println(vect[position]);
-		}
-		catch (ArrayIndexOutOfBoundsException e) {
+		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Invalid position!");
-		}
-		catch (InputMismatchException e) {
+		} catch (InputMismatchException e) {
 			System.out.println("Input error");
 		}
-		
+
 		System.out.println("End of program");
-			
-		
+
 		sc.close();
 	}
 
